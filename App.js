1d1e6328777acca5ec88b07a84d2db4cb98f8c0f
nikreal <nikreal885@gmail.com>
2019-04-15 11:49:28 -0700

    add splash screen

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import SplashScreen from './components/SplashScreen';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,7 +19,39 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  // We set the isLoading state to true, that will indicate that we are loading essential data. Once we have done, we’ll turn the flag to false.
+  constructor(props) {
+    super(props);  
+    this.state = { isLoading: true }
+  }
+
+  // For time-consuming task of SplashScreen
+  performTimeConsumingTask = async() => {
+    return new Promise((resolve) =>
+      setTimeout(
+        () => { resolve('result') },
+        2000
+      )
+    );
+  }
+
+  async componentDidMount() {
+    // Preload data from an external API
+    // Preload data using AsyncStorage
+    const data = await this.performTimeConsumingTask();
+  
+    if (data !== null) {
+      this.setState({ isLoading: false });
+    }
+  }
+
   render() {
+    // When data is still loading, we are going to render the SplashScreen component instead of the application body.
+    if (this.state.isLoading) {
+      return <SplashScreen />
+    }
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
---
+++ components/SplashScreen.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Image} from 'react-native';
+
+export default class SplashScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require('../resources/images/mark.png')}
+          style={styles.logoStyle}
+          resizeMode='contain'
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#4D207F',
+  },
+  logoStyle: {
+    width: '80%',
+  },
+});
