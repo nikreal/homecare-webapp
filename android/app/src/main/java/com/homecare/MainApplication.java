00d85bc3dfe196dc2072a8c3564651951ff40058
nikreal <nikreal885@gmail.com>
2019-05-21 07:05:54 -0700

    react-native@0.57.1 react-native-webview@5.8.1

--- android/app/src/main/java/com/homecare/MainApplication.java
+++ android/app/src/main/java/com/homecare/MainApplication.java
@@ -4,9 +4,9 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.cancue.RNWebViewCleaner.RNWebViewCleanerPackage;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -28,9 +28,9 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNWebViewCleanerPackage(),
-            new RNCWebViewPackage(),
             new VectorIconsPackage(),
-            new RNGestureHandlerPackage()
+            new RNGestureHandlerPackage(),
+            new RNCWebViewPackage()
       );
     }
 
